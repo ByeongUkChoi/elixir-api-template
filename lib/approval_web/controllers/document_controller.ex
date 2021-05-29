badac0a8bdcd8b80b91dc83c1391528a482cd10c
ByeongUk Choi <cbw0916@gmail.com>
2021-05-30 00:06:03 +0900

    Add response draft document

--- lib/approval_web/controllers/document_controller.ex
+++ lib/approval_web/controllers/document_controller.ex
@@ -22,50 +22,28 @@ defmodule ApprovalWeb.DocumentController do
   end
 
   def draft(conn, params) do
-    # case 1
-    # Repo.transaction(fn ->
-    #   with {:ok, %Document{} = document} <- Repo.insert(%Document{
-    #     title: params["title"],
-    #     content: params["content"],
-    #     drafter_id: get_req_header(conn, "x-user-id") |> hd |> String.to_integer(),
-    #     drafter_opinion: params["opinion"],
-    #   }),
-    #   {_approval_lines_count, nil} <- Repo.insert_all(ApprovalLine, Enum.map(params["approveLines"], fn(approve_line) -> %{
-    #       document_id: document.id,
-    #       sequence: approve_line["sequence"],
-    #       approver_id: approve_line["approverId"],
-    #       approval_type: approve_line["approvalType"]
-    #   } end))
-    #   do
-    #     send_resp(conn, :created, document.title)
-    #   end
-    # end)
+    {:ok, %{approval_lines_insert_all: _insert_all, document: document}} = Multi.new()
+    |> Multi.insert(:document, %Document{
+      title: params["title"],
+      content: params["content"],
+      drafter_id: get_req_header(conn, "x-user-id") |> hd |> String.to_integer(),
+      drafter_opinion: params["opinion"],
+    })
+    |> Multi.merge(fn %{document: document} ->
+      Multi.new()
+      |> Multi.insert_all(:approval_lines_insert_all, ApprovalLine, Enum.map(params["approveLines"], fn(approve_line) -> %{
+        document_id: document.id,
+        sequence: approve_line["sequence"],
+        approver_id: approve_line["approverId"],
+        approval_type: approve_line["approvalType"]
+        # TODO: received_at
+    } end))
+    end)
+    |> Repo.transaction()
 
-    Multi.new()
-      |> Multi.insert(:document, %Document{
-        title: params["title"],
-        content: params["content"],
-        drafter_id: get_req_header(conn, "x-user-id") |> hd |> String.to_integer(),
-        drafter_opinion: params["opinion"],
-      })
-      |> Multi.merge(fn %{document: document} ->
-        Multi.new()
-        |> Multi.insert_all(:approval_lines_insert_all, ApprovalLine, Enum.map(params["approveLines"], fn(approve_line) -> %{
-          document_id: document.id,
-          sequence: approve_line["sequence"],
-          approver_id: approve_line["approverId"],
-          approval_type: approve_line["approvalType"]
-      } end))
-      end)
-      |> Repo.transaction()
-    send_resp(conn, :created, '')
-  end
-  def draft2(conn, params) do
-    %Document{}
-    |> Document.changeset(params)
-    |> Repo.insert()
     conn
     |> put_status(:created)
+    |> show(%{"id" => document.id})
   end
 
   def create(conn, %{"document" => document_params}) do

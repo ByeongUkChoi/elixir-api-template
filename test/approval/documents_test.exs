5c2c41563e0bbb7cb3fec2fbdc26f3dfb94b77af
ByeongUk Choi <cbw0916@gmail.com>
2021-09-21 00:57:14 +0900

    Add confirm document test function

--- test/approval/documents_test.exs
+++ test/approval/documents_test.exs
@@ -7,15 +7,37 @@ defmodule Approval.DocumentsTest do
   alias Approval.Documents.Document
 
   describe "documents context test" do
-
     @valid_attrs %{
       title: "some title",
       content: "some content",
       drafter_id: 42,
       drafter_opinion: "some drafter_opinion",
       status: "ON_PROGRESS",
+      inserted_at: ~N[2000-01-01 23:00:07],
+      updated_at: ~N[2000-01-01 23:00:07],
       approval_lines: [%{sequence: 1, approver_id: 11, received_at: ~N[2000-01-01 23:00:07]}]
     }
+    @document_with_last_approval_line %{
+      title: "some title",
+      content: "some content",
+      drafter_id: 42,
+      drafter_opinion: "some drafter_opinion",
+      status: "ON_PROGRESS",
+      inserted_at: ~N[2000-01-01 23:00:07],
+      updated_at: ~N[2000-01-01 23:00:07],
+      approval_lines: [%{sequence: 1, approver_id: 11, received_at: ~N[2000-01-01 23:00:07]}]
+    }
+
+    # @document_with_not_last_approval_line %{
+    #   title: "some title",
+    #   content: "some content",
+    #   drafter_id: 42,
+    #   drafter_opinion: "some drafter_opinion",
+    #   status: "ON_PROGRESS",
+    #   inserted_at: ~N[2000-01-01 23:00:07],
+    #   updated_at: ~N[2000-01-01 23:00:07],
+    #   approval_lines: [%{sequence: 1, approver_id: 11, received_at: ~N[2000-01-01 23:00:07]}, %{sequence: 2, approver_id: 12}]
+    # }
     # @update_attrs %{content: "some updated content", drafter_id: 43, drafter_opinion: "some updated drafter_opinion", title: "some updated title"}
     # @invalid_attrs %{content: nil, drafter_id: nil, drafter_opinion: nil, title: nil}
 
@@ -52,7 +74,30 @@ defmodule Approval.DocumentsTest do
       assert @valid_attrs = document
     end
 
-    test "approve :confirm" do
+    test "confirm/3 document without remaining approval line" do
+      # given
+      %{id: document_id} = document_fixture(@document_with_last_approval_line)
+
+      %{approver_id: approver_id} =
+        @document_with_last_approval_line.approval_lines
+        |> Enum.reverse()
+        |> Enum.find(&(!is_nil(&1.received_at)))
+
+      approval_opinion = "confirm document!!!!"
+
+      # when
+      {:ok, document} = Documents.confirm(document_id, approver_id, approval_opinion)
+
+      # then
+      assert CONFIRMED == document.status
+
+      assert %{approval_opinion: approval_opinion} =
+               document.approval_lines
+               |> Enum.reverse()
+               |> Enum.find(&(&1.approver_id == ^approver_id))
     end
+
+    # test "confirm/3 document with remaining approval line" do
+    # end
   end
 end

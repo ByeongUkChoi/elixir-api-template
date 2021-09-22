6ba298ac6e97a222cc9aced1b2f0901973d8e0fa
ByeongUk Choi <cbw0916@gmail.com>
2021-09-22 22:31:29 +0900

    Add Documents.confirm/3 test

--- test/approval/documents_test.exs
+++ test/approval/documents_test.exs
@@ -27,17 +27,17 @@ defmodule Approval.DocumentsTest do
       updated_at: ~N[2000-01-01 23:00:07],
       approval_lines: [%{sequence: 1, approver_id: 11, received_at: ~N[2000-01-01 23:00:07]}]
     }
+    @document_with_middle_approval_line %{
+      title: "some title",
+      content: "some content",
+      drafter_id: 42,
+      drafter_opinion: "some drafter_opinion",
+      status: "ON_PROGRESS",
+      inserted_at: ~N[2000-01-01 23:00:07],
+      updated_at: ~N[2000-01-01 23:00:07],
+      approval_lines: [%{sequence: 1, approver_id: 11, received_at: ~N[2000-01-01 23:00:07]}, %{sequence: 1, approver_id: 11}]
+    }
 
-    # @document_with_not_last_approval_line %{
-    #   title: "some title",
-    #   content: "some content",
-    #   drafter_id: 42,
-    #   drafter_opinion: "some drafter_opinion",
-    #   status: "ON_PROGRESS",
-    #   inserted_at: ~N[2000-01-01 23:00:07],
-    #   updated_at: ~N[2000-01-01 23:00:07],
-    #   approval_lines: [%{sequence: 1, approver_id: 11, received_at: ~N[2000-01-01 23:00:07]}, %{sequence: 2, approver_id: 12}]
-    # }
     # @update_attrs %{content: "some updated content", drafter_id: 43, drafter_opinion: "some updated drafter_opinion", title: "some updated title"}
     # @invalid_attrs %{content: nil, drafter_id: nil, drafter_opinion: nil, title: nil}
 
@@ -94,10 +94,31 @@ defmodule Approval.DocumentsTest do
       assert %{approval_opinion: approval_opinion} =
                document.approval_lines
                |> Enum.reverse()
-               |> Enum.find(&(&1.approver_id == ^approver_id))
+               |> Enum.find(&(&1.approver_id == approver_id))
     end
 
-    # test "confirm/3 document with remaining approval line" do
-    # end
+    test "confirm/3 document with remaining approval line" do
+      # given
+      %{id: document_id} = document_fixture(@document_with_middle_approval_line)
+
+      %{approver_id: approver_id} =
+        @document_with_middle_approval_line.approval_lines
+        |> Enum.reverse()
+        |> Enum.find(&(!is_nil(&1.received_at)))
+
+      approval_opinion = "confirm document!!!!"
+
+      # when
+      {:ok, document} = Documents.confirm(document_id, approver_id, approval_opinion)
+
+      # then
+      assert ON_PROGRESS == document.status
+
+      assert %{approval_opinion: approval_opinion} =
+               document.approval_lines
+               |> Enum.reverse()
+               |> Enum.find(&(&1.approver_id == approver_id))
+      # TODO: check next approver receive date
+    end
   end
 end

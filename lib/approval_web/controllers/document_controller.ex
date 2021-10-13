968a8aaf5489d5d1c81973570aef81a13a583ce1
ByeongUk Choi <cbw0916@gmail.com>
2021-10-14 01:06:26 +0900

    Fix document_controller.draft/2 request params

--- lib/approval_web/controllers/document_controller.ex
+++ lib/approval_web/controllers/document_controller.ex
@@ -20,6 +20,14 @@ defmodule ApprovalWeb.DocumentController do
   end
 
   def draft(conn, params) do
+    drafter_id = get_req_header(conn, "x-user-id") |> hd |> String.to_integer()
+
+    params =
+      params
+      |> Map.put("drafter_id", drafter_id)
+      # TODO: camel case to snake case
+      |> Map.put("drafter_opinion", params["drafterOpinion"])
+
     {:ok, document} = Documents.draft_document(params)
 
     conn
